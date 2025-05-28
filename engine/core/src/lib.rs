3dc0d66d40c7293e56a28aee157ea383da5b828c
Jen Eleniel <>
2025-05-28 12:19:12 -0400

    Added tracing and output to std*

--- engine/core/src/lib.rs
+++ engine/core/src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use tracing::{info, instrument};
+
+#[instrument]
+pub async fn run() {
+    info!("Core initialized");
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod tests {}
